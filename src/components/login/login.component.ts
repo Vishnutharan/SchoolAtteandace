f825e07d45c29a9b76ccb5cc69d07f52543d8497
Vishnutharan <karunakaran.vishnu6@gmail.com>
2024-10-26 01:43:35 +0530

    fifth_Commi

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,4 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +14,29 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
+  fb = inject(FormBuilder);
+  loginForm: FormGroup;
+  hidePassword = true;
 
+  constructor() {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe({
+        next: () => {
+          this.router.navigate(['/home']);
+        },
+        error: (error) => {
+          console.error('Login failed:', error);
+        },
+      });
+    }
+  }
 }
